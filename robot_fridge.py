b3fd346a3401d07fc11a085def2e06a2c29a59d7
Olmo Kramer <olmo@codegrade.com>
2021-03-06 16:31:13 +0100

    Add robot_fridge.py

---
+++ robot_fridge.py
@@ -0,0 +1,19 @@
+from adafruit_motorkit import MotorKit
+
+def move_forward(kit: MotorKit, throttle: float = 1):
+    kit.motor1.throttle = throttle
+    kit.motor2.throttle = throttle
+    kit.motor3.throttle = throttle
+    kit.motor4.throttle = throttle
+
+def move_backward(kit: MotorKit, throttle: float = 1):
+    move_forward(kit, -throttle)
+
+def rotate_left(kit: MotorKit, throttle: float = 1):
+    kit.motor1.throttle = throttle
+    kit.motor2.throttle = throttle
+    kit.motor3.throttle = -throttle
+    kit.motor4.throttle = -throttle
+
+def rotate_right(kit: MotorKit, throttle: float = 1):
+    rotate_left(kit, -throttle)
