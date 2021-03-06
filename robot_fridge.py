cfa62ef1fbb293b8bdc1b6be7a4dcc3da0def1c4
Willem <robotfridge@willem.nl>
2021-03-06 23:08:00 +0100

    Added return types

--- robot_fridge.py
+++ robot_fridge.py
@@ -1,23 +1,23 @@
 from adafruit_motorkit import MotorKit  # type: ignore
 
 
-def move_forward(kit: MotorKit, throttle: float = 1):
+def move_forward(kit: MotorKit, throttle: float = 1) -> None:
     kit.motor1.throttle = throttle
     kit.motor2.throttle = throttle
     kit.motor3.throttle = throttle
     kit.motor4.throttle = throttle
 
 
-def move_backward(kit: MotorKit, throttle: float = 1):
+def move_backward(kit: MotorKit, throttle: float = 1) -> None:
     move_forward(kit, -throttle)
 
 
-def rotate_left(kit: MotorKit, throttle: float = 1):
+def rotate_left(kit: MotorKit, throttle: float = 1) -> None:
     kit.motor1.throttle = throttle
     kit.motor2.throttle = throttle
     kit.motor3.throttle = -throttle
     kit.motor4.throttle = -throttle
 
 
-def rotate_right(kit: MotorKit, throttle: float = 1):
+def rotate_right(kit: MotorKit, throttle: float = 1) -> None:
     rotate_left(kit, -throttle)
