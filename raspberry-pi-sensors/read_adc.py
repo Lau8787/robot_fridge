ae334eee64a9fe1060c45385cbcf6c4e1cc6a058
Olmo Kramer <olmo@codegrade.com>
2021-03-07 15:53:27 +0100

    Fix flake8 errors

--- raspberry-pi-sensors/read_adc.py
+++ raspberry-pi-sensors/read_adc.py
@@ -32,7 +32,12 @@ def main() -> None:
         # print("{:>5}\t{:>5.3f}".format(x_axis.value, x_axis.voltage))
         # print("{:>5}\t{:>5.3f}".format(y_axis.value, y_axis.voltage))
 
-        print(f"P0: {p0.value}; P1-axis: {p1.value}; P2-axis: {p12value}; P3-axis: {p3.value}")
+        print('; '.join([
+            f'P0: {p0.value}',
+            f'P1: {p1.value}',
+            f'P2: {p2.value}',
+            f'P3: {p3.value}',
+        ]))
 
         time.sleep(0.005)
 
