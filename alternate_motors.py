a088ff7b24227e3e4a700437131915ff321855e1
Olmo Kramer <olmo@codegrade.com>
2021-03-13 16:22:17 +0000

    Add RobotFridge class and alternate_motors script

---
+++ alternate_motors.py
@@ -0,0 +1,28 @@
+import sys
+import time
+
+from adafruit_motorkit import MotorKit  # type: ignore
+
+from robot_fridge import RobotFridge
+
+
+def main(argv):
+    kit = MotorKit()
+    bot = RobotFridge.from_kit(kit)
+
+    try:
+        while True:
+            bot.move_forward()
+            time.sleep(1)
+            bot.move_backward()
+            time.sleep(1)
+            bot.rotate_left()
+            time.sleep(1)
+            bot.rotate_right()
+            time.sleep(1)
+    finally:
+        bot.halt()
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
