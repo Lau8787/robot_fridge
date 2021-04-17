561603af0aaf4af83f9fd469d83dc5565d733594
Olmo Kramer <olmo@codegrade.com>
2021-04-17 23:45:25 +0100

    Added script for controlling robot with arrow keys

---
+++ robot_fridge/__main__.py
@@ -0,0 +1,40 @@
+import curses
+
+from robot_fridge import RobotFridge, MotorKit
+
+ACTIONS = {
+    'KEY_UP': 'move_forward',
+    'KEY_DOWN': 'move_backward',
+    'KEY_RIGHT': 'rotate_right',
+    'KEY_LEFT': 'rotate_left',
+    ' ': 'halt',
+}
+
+def main(win):
+    bot = RobotFridge.from_kit(MotorKit())
+
+    win.nodelay(True)
+
+    try:
+        while True:          
+            try:                 
+                key = win.getkey()         
+                win.clear()                
+
+                if key == '\x1b':
+                    win.addstr('stop')
+                    break           
+
+                action = ACTIONS.get(key)
+                if action is not None:
+                    win.addstr(action)
+                    getattr(bot, action)()
+            except Exception as e:
+                # No input   
+                pass 
+    finally:
+        bot.halt()
+
+
+if __name__ == '__main__':
+    curses.wrapper(main)
